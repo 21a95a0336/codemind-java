34a508ffe1518cb577e798bbed074f339e2074a9
Manikanta Mani <102538903+21a95a0336@users.noreply.github.com>
2022-08-12 11:04:42 +0530

    import java .util.Scanner; class tria{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n,r,v;         n=sc.nextInt();         r=sc.nextInt();         v=sc.nextInt();         float a,s;         s=(float)(n+r+v)/2;          a=(float) Math.sqrt((s)*(s-n)*(s-r)*(s-v));
            System.out.format("%.2f",a);
        }
    }

---
+++ Area_of_Triangle_.java
@@ -0,0 +1,16 @@
+import java .util.Scanner;
+class tria{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n,r,v;
+        n=sc.nextInt();
+        r=sc.nextInt();
+        v=sc.nextInt();
+        float a,s;
+        s=(float)(n+r+v)/2;
+         a=(float) Math.sqrt((s)*(s-n)*(s-r)*(s-v));
+         
+        System.out.format("%.2f",a);
+    }
+}
