8c7f1b37503dc3b561c41ea2db866f0c86e17f7f
Manikanta Mani <102538903+21a95a0336@users.noreply.github.com>
2022-08-27 14:00:22 +0530

    import java.util.Scanner; class sample{     public static void main(String args[]){     Scanner sc=new Scanner(System.in);     int t,a,b,sum;     t=sc.nextInt();     while(t>0)     {         a=sc.nextInt();         b=sc.nextInt();         sum=(a+b);         System.out.println(sum);
            t--;
        }
      }
    }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class sample{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int t,a,b,sum;
+    t=sc.nextInt();
+    while(t>0)
+    {
+        a=sc.nextInt();
+        b=sc.nextInt();
+        sum=(a+b);
+        System.out.println(sum);
+        
+        t--;
+    }
+  }
+}
