8898032cce0222fbd75a40d7b4feb7b166c03514
Manikanta Mani <102538903+21a95a0336@users.noreply.github.com>
2022-10-29 14:52:18 +0530

    import java.util.Scanner; class sample{     public static void main(String [] args){         Scanner sc=new Scanner (System.in);     String n=sc.nextLine();     String words[]=n.split(" ");
        System.out.println(words.length);
        }
        
        
    }

---
+++ Count_no._of_words.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class sample{
+    public static void main(String [] args){
+        Scanner sc=new Scanner (System.in);
+    String n=sc.nextLine();
+    String words[]=n.split(" ");
+    
+    System.out.println(words.length);
+    }
+    
+    
+}
