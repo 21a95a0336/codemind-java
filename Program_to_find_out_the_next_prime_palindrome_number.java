f539a19f0be766b856d94e267ec85e6848af64c1
Manikanta Mani <102538903+21a95a0336@users.noreply.github.com>
2022-11-04 15:35:52 +0530

    import java.util.*; class nextprimepalin{     public static boolean palindrome(int x){         int rev=0,r;         int i=x;         while (x>0)         {            r=x%10;            rev=rev*10+r;            x=x/10;         }         return rev==i;     }     public static boolean isprime(int y){         int count=0;         for(int i=1;i<=y;i++)         {             if(y%i==0)             count++;         }         return count==2;     }     public static int nextpalindrome(int z){         if (palindrome(z)==true)z++;         while(palindrome(z)==false){             z++;         }return z;
        }
        public static int nextppalin(int x){
            if (isprime(x)==true)x++;
            while(isprime(x)!=true){
               x = nextpalindrome(x);
            }return x;
        }
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            int c;
            c=sc.nextInt();
            System.out.println(nextppalin(c));
        }
    }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,44 +1,43 @@
-import java.util.Scanner;
-class sample{
-    public static boolean isprime(int n){
-        int count=0;
-        for (int i=2;i<=(int)Math.sqrt(n);i++)
+import java.util.*;
+class nextprimepalin{
+    public static boolean palindrome(int x){
+        int rev=0,r;
+        int i=x;
+        while (x>0)
         {
-            if (n%i==0)
+           r=x%10;
+           rev=rev*10+r;
+           x=x/10;
+        }
+        return rev==i;
+    }
+    public static boolean isprime(int y){
+        int count=0;
+        for(int i=1;i<=y;i++)
+        {
+            if(y%i==0)
             count++;
         }
-        if (count==0)
-            return true;
-        else
-            return false;
+        return count==2;
     }
-    public static boolean palindrome(int n){
-        int p=n;
-        int r,rev=0;
-        while(n>0)
-        {
-            r=n%10;
-            rev=rev*10+r;
-            n=n/10;
-        }
-        if (p==rev)
-        return true;
-        else
-        return false;
+    public static int nextpalindrome(int z){
+        if (palindrome(z)==true)z++;
+        while(palindrome(z)==false){
+            z++;
+        }return z;
+            
+        
     }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int m,i;
-        m=sc.nextInt();
-        for(i=m+1;;i++)
-        {
-            if(isprime(i) && palindrome(i))
-            {
-                System.out.println(i);
-                            break;
-            }
-        }
+    public static int nextppalin(int x){
+        if (isprime(x)==true)x++;
+        while(isprime(x)!=true){
+           x = nextpalindrome(x);
+        }return x;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int c;
+        c=sc.nextInt();
+        System.out.println(nextppalin(c));
     }
 }
-    
-    
